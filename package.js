fd6d0d0d578dafa042a45fc0b1352c50158c4d2b
Brent Abrahams <brent_abrahams@yahoo.com>
2016-09-19 00:00:27 +0800

    Fixed bug in clickable links

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg',
   summary: 'Don\'t add this - done automatically when adding a wysiwyg editor to babrahams:editable-text',
-  version: '0.5.24',
+  version: '0.5.25',
   git: 'https://github.com/JackAdams/meteor-editable-text-wysiwyg'
 });
 
