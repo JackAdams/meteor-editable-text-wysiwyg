f0130def11f67a666e1e029d1c77af9f4bd76456
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:13:23 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,23 +1,20 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg',
   summary: 'Don\'t add this - done automatically when adding a wysiwyg editor to babrahams:editable-text',
-  version: '0.6.15',
+  version: '0.6.21',
   git: 'https://github.com/JackAdams/meteor-editable-text-wysiwyg'
 });
 
 Package.onUse(function(api) {
     
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
   
-  api.use('babrahams:editable-text@0.9.12', 'client');
-  api.use('templating', 'client');
-  api.use('blaze', 'client');
-  api.use('spacebars', 'client');
-  api.use('jquery', 'client');
+  api.use('babrahams:editable-text@0.9.17', 'client');
+  api.use(['templating@1.3.2', 'blaze@2.3.4', 'spacebars@1.0.15', 'jquery@1.11.11'], 'client');
   api.use('underscore', 'client');
   api.use('reactive-var', 'client');
   
-  api.add_files('lib/bootstrap_wysiwyg.js', 'client');
+  api.addFiles('lib/bootstrap_wysiwyg.js', 'client');
   api.addFiles('lib/wysiwyg.css', 'client');
   api.addFiles('lib/wysiwyg.html', 'client');
   api.addFiles('lib/wysiwyg.js', 'client');
